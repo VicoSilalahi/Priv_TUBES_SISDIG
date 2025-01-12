8f018ccd946201b183379f1a2f1d8e208afac848
VicoSilalahi <vicoco.lahi@gmail.com>
2025-01-12 10:22:32 +0700

    Clean Up

--- Dev_Unused/UART/runTXTB.ps1
+++
@@ -1,7 +0,0 @@
-ghdl --remove
-ghdl -a --std=02 --work=work .\UART_RX.vhd .\UART_TX.vhd .\TOP.vhd
-ghdl -e --std=02 --work=work UART_RX
-ghdl -e --std=02 --work=work UART_TX
-ghdl -a --std=02 --work=work UART_TX_tb.vhd
-ghdl -r --std=02 --work=work UART_TX_tb --wave=wave.ghw
-# gtkwave .\wave.ghw &
--- Dev_Unused/oldsrc/runlea128.ps1
+++
@@ -1,4 +0,0 @@
-ghdl --remove
-ghdl -a --std=02 --work=work *.vhd
-ghdl -e --std=02 --work=work cypher_block_tb
-ghdl -r --std=02 --work=work cypher_block_tb --wave=wave.ghw
